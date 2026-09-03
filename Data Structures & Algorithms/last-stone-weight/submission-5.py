7215ace495042de858da229ec373b773ed145ea2
Jaya Shruti Chintalapati <jayashrutichintalapati@gmail.com>
2026-09-03 17:53:27 -0400

    Add: last-stone-weight - submission-5

---
+++ Data Structures & Algorithms/last-stone-weight/submission-5.py
@@ -0,0 +1,28 @@
+class Solution:
+    def lastStoneWeight(self, stones: List[int]) -> int:
+        m=max(stones)
+        buckets=[0]*(m+1)
+        for s in stones:
+            buckets[s] += 1
+        first=second=m
+        while first>0:
+            if buckets[first]%2==0:
+                first-=1
+                continue
+            j=min(first-1, second)
+            while j>0 and buckets[j]==0:
+                j-=1
+            if j==0:
+                return first
+            second=j
+            buckets[second]-=1
+            buckets[first]-=1
+            buckets[first-second]+=1
+            first=max(first-second,second)
+        return first
+
+            
+            
+
+        
+        
