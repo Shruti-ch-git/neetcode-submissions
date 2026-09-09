43a56d375e54e7af5ae3c62e87d0a782d831267b
Jaya Shruti Chintalapati <jayashrutichintalapati@gmail.com>
2026-09-09 15:13:22 -0400

    Add: meeting-schedule - submission-2

---
+++ Data Structures & Algorithms/meeting-schedule/submission-2.py
@@ -0,0 +1,11 @@
+class Solution:
+    def canAttendMeetings(self, intervals: List[Interval]) -> bool:
+        intervals.sort(key=lambda interval: interval.start)
+
+        for i in range(1, len(intervals)):
+            previous_end = intervals[i - 1].end
+            current_start = intervals[i].start
+
+            if current_start < previous_end:
+                return False
+        return True
